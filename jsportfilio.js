35a00f5400fc076aa2eda3151e65faf13f5dfa5b
jacobitkashala <jacobitkashala@gmail.com>
2021-04-09 15:45:17 +0200

    mis a jour

--- jsportfilio.js
+++ jsportfilio.js
@@ -1,6 +1,12 @@
 const ulrData = "https://jacobitkashala.github.io/Data-portfilio.json";
 
 let userNom = document.getElementById("Nom");
+let userDate = document.getElementById("datenaiss");
+let userVille = document.getElementById("ville");
+let userPays = document.getElementById("pays");
+let userNiveau = document.getElementById("niveau");
+let userTelephone = document.getElementById("telephone");
+
 // console.log(userNom);
 
 fetch(ulrData)
@@ -8,6 +14,12 @@ fetch(ulrData)
         return response.json();
     })
     .then(dbase => {
+        userNom.innerText = dbase.name;
+        userDate.innerText = dbase.datenais;
+        userVille.innerText = dbase.ville;
+        userPays.innerText = dbase.pays;
+        userNiveau.innerText = dbase.niveau;
+        userTelephone.innerText = dbase.niveau;
 
-        console.log(dbase.name);
+        // console.log(dbase.name);
     })
