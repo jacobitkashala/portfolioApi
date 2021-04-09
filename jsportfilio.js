86d09dd4ff66b7cd6c0f1e0b72421f88bed9a5e1
jacobitkashala <jacobitkashala@gmail.com>
2021-04-09 12:10:24 +0200

    portofolio 09 04 2021

---
+++ jsportfilio.js
@@ -0,0 +1,17 @@
+let closed = document.getElementById("closedId");
+let check = document.getElementById("checkId");
+let btn = document.getElementById("btnid")
+
+// console.dir(check);
+// console.dir(btn);
+
+// check.onclick = function() {
+//         closed.style.visibility = "visible";
+//         check.style.visibility = "hidden";
+
+//         console.dir(closed.style);
+//     }
+// closed.onclick = function() {
+//     console.log("la clo");
+
+// }
