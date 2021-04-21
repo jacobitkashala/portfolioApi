9e1a1704a52be7964d9ad6365ececb90c419eda2
jacobitkashala <jacobitkashala@gmail.com>
2021-04-21 15:36:44 +0200

    mise a jour responsive

--- jsportfilio.js
+++ jsportfilio.js
@@ -1,5 +1,6 @@
 const ulrData = "https://jacobitkashala.github.io/Data-portfilio.json";
 
+
 let userNom = document.getElementById("Nom");
 let userDate = document.getElementById("datenaiss");
 let userVille = document.getElementById("ville");
@@ -9,17 +10,47 @@ let userTelephone = document.getElementById("telephone");
 
 // console.log(userNom);
 
-fetch(ulrData)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(dbase => {
-        userNom.innerText = dbase.name;
-        userDate.innerText = dbase.datenais;
-        userVille.innerText = dbase.ville;
-        userPays.innerText = dbase.pays;
-        userNiveau.innerText = dbase.niveau;
-        userTelephone.innerText = dbase.niveau;
+let competenceHtmlLabel = document.getElementById("competence--html");
+let competenceCssLabel = document.getElementById("competence--css");
+let competenceJvaLabel = document.getElementById("competence--jva");
+// let competenceJvaLabel = document.getElementById("competence--cs");
 
-        // console.log(dbase.name);
-    })
+let competenceHtml = document.getElementById("progress--css");
+let competenceCss = document.getElementById("progress--css");
+let competenceJva = document.getElementById("progress--jva");
+let competenceCs = document.getElementById("progress--cs");
+
+window.addEventListener('scroll', function(e) {
+    //     // derniere_position_de_scroll_connue = window.scrollY;
+
+    //     // if (!ticking) {
+    //     //     window.requestAnimationFrame(function() {
+    //     //         faireQuelqueChose(derniere_position_de_scroll_connue);
+    //     //         ticking = false;
+    //     //     });
+    // }
+
+    // ticking = true;
+    console.log("scroll");
+
+});
+
+window.addEventListener('scroll', () => {
+    console.log("scroll");
+});
+
+
+// fetch(ulrData)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(dbase => {
+//         userNom.innerText = dbase.name;
+//         userDate.innerText = dbase.datenais;
+//         userVille.innerText = dbase.ville;
+//         userPays.innerText = dbase.pays;
+//         userNiveau.innerText = dbase.niveau;
+//         userTelephone.innerText = dbase.niveau;
+
+//         // console.log(dbase.name);
+//     })
